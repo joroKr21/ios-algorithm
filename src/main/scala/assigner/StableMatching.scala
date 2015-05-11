f7b19ba60995c208ed859542d6502a152fbbbc67
marcromeyn <marcromeyn@gmail.com>
2015-05-11 21:15:40 +0200

    Made the code a little more readable

--- src/main/scala/assigner/StableMatching.scala
+++ src/main/scala/assigner/StableMatching.scala
@@ -1,29 +1,28 @@
 package assigner
 
-import scala.util.Random
-
 class StableMatching(students: List[Student], groups: List[Group]) extends MatchingSolution(students, groups) {
   val totalSize = groups.map(_.size).sum
-  val mandatoryStudents = students.filter(_.mandatory == true)
-  val otherStudents = Random.shuffle(students.filter(_.mandatory == false))
+  val mandatoryStudents = students.filter(_.mandatory)
+  val otherStudents = students.filter(!_.mandatory).shuffle
   val selectedStudents = mandatoryStudents ++ otherStudents.take(totalSize - mandatoryStudents.size)
 
   matching = {
-    Random.shuffle(selectedStudents).map { student =>
+    selectedStudents.shuffle.map { student =>
       var finished = false
       var group = 0
-      var id = 0
+      var studentId = 0
+
       while(!finished) {
         val assigned = groups(group).addStudent(student)
         if(assigned) {
           finished = true
-          id  = student.id
+          studentId  = student.id
         } else {
           group += 1
         }
       }
 
-      id -> group
+      studentId -> group
     }.toMap
   }
 }
