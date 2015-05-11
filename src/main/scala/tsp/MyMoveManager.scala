be1aa9079317c6c031bddc983530d158f9c0eacb
marcromeyn <marcromeyn@gmail.com>
2015-05-11 14:59:09 +0200

    Algorithm is running now in scala, only a minor difference with the java implementation (swaps two numbers)

--- src/main/scala/tsp/MyMoveManager.scala
+++ src/main/scala/tsp/MyMoveManager.scala
@@ -7,20 +7,20 @@ class MyMoveManager extends MoveManager {
     case solution:MySolution => {
       val tour = solution.tour
 
-      val buffer = new Array[Move](tour.length * tour.length)
+      val buffer = Array.ofDim[Move](tour.length * tour.length)
       var nextBufferPos: Int = 0
 
       for {
         i <- 1 until tour.length
-        j <- -5 until 5
+        j <- -5 until 6
         if (i + j >= 1) && (i + j < tour.length) && (j != 0)
       } {
-        nextBufferPos += 1
         buffer(nextBufferPos) = new MySwapMove(tour(i), j)
+        nextBufferPos += 1
       }
 
       // Trim buffer
-      val moves = new Array[Move](nextBufferPos)
+      val moves = Array.ofDim[Move](nextBufferPos)
       System.arraycopy(buffer, 0, moves, 0, nextBufferPos)
 
       moves
--- src/main/scala/tsp/MySolution.scala
+++ src/main/scala/tsp/MySolution.scala
@@ -4,14 +4,26 @@ import org.coinor.opents.SolutionAdapter
 
 
 case class MySolution(customers: Array[Array[Double]]) extends SolutionAdapter {
-  val tour = (0 until customers.length).toArray
+  var tour = (0 until customers.length).toArray
+
+  override def clone = {
+    val copy: MySolution = super.clone.asInstanceOf[MySolution]
+    copy.tour = tour
+
+    copy
+  }
 
   override def toString: String = {
     val s: StringBuffer = new StringBuffer
     s.append("Solution value: " + getObjectiveValue()(0))
     s.append(" Sequence: [ ")
-    tour.map(x => s.append(x).append(", "))
-    s.append(tour(tour.length - 1))
+    tour.map { x =>
+      if (x == 0) {
+        s.append(x)
+      } else {
+        s.append(", ").append(x)
+      }
+    }
     s.append(" ]")
 
     s.toString
