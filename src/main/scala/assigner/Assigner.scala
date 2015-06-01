0346a2546b63307961481fc0f696b4f2c5be24ff
marcromeyn <marcromeyn@gmail.com>
2015-06-01 14:13:32 +0200

    Added Friends and Foes in the objective function

--- src/main/scala/assigner/Assigner.scala
+++ src/main/scala/assigner/Assigner.scala
@@ -8,7 +8,7 @@ object Assigner extends App {
     Student(0, "dss", true, Map("1" -> 5, "2" -> 3, "3" -> 4), List(2, 1, 0), Set(4), Set()),
     Student(1, "dss", true, Map("1" -> 1, "2" -> 5, "3" -> 2), List(1, 0, 2), Set(), Set(7)),
     Student(2, "dss", true, Map("1" -> 4, "2" -> 5, "3" -> 3), List(0, 2, 1), Set(), Set()),
-    Student(3, "dss", true, Map("1" -> 2, "2" -> 4, "3" -> 5), List(0, 1, 2), Set(1), Set()),
+    Student(3, "dss", true, Map("1" -> 2, "2" -> 4, "3" -> 5), List(0, 1, 2), Set(1,2,4,5,6,7,8,9), Set()),
     Student(4, "dss", true, Map("1" -> 4, "2" -> 4, "3" -> 3), List(2, 1, 0), Set(), Set()),
     Student(5, "dss", true, Map("1" -> 3, "2" -> 3, "3" -> 3), List(1, 2, 0), Set(3, 8), Set()),
     Student(6, "dss", true, Map("1" -> 1, "2" -> 4, "3" -> 3), List(0, 1, 2), Set(), Set()),
--- src/main/scala/assigner/Objective.scala
+++ src/main/scala/assigner/Objective.scala
@@ -1,20 +1,14 @@
 package assigner
 
-import org.coinor.opents.{Move, Solution, ObjectiveFunction}
+import org.coinor.opents.{Move, ObjectiveFunction, Solution}
 
 case class Objective(students: Map[Int, Student], groups: Map[Int, Group]) extends ObjectiveFunction {
   override def evaluate(solution: Solution, move: Move): Array[Double] = {
     def eval(solution: Solution) = solution match {
       case sol: Assignment =>
-        val minSkills = sol.groupMap.flatMap {
-          case (g, ss) => groups(g).skills.map { skill =>
-            skill -> ss.map(s => students(s).skills(skill)).max
-          }.toMap
-        }.groupBy(_._1).mapValues(_.values.min)
-
-        val Gs = minSkills.values.min * minSkills.values.sum
-        println(Gs)
-        Array(Gs.toDouble)
+        val G = Gs(sol)
+        val F = Fs(sol)
+        Array(G+F)
     }
 
     if (move == null) eval(solution)
@@ -24,4 +18,48 @@ case class Objective(students: Map[Int, Student], groups: Map[Int, Group]) exten
       eval(newSol)
     }
   }
+
+  def Gs(solution: Assignment) : Double = {
+    val minSkills = solution.groupMap.flatMap {
+      case (g, ss) => groups(g).skills.map { skill =>
+        skill -> ss.map(s => students(s).skills(skill)).max
+      }.toMap
+    }.groupBy(_._1).mapValues(_.values.min)
+
+    val Gs = minSkills.values.min * minSkills.values.sum
+    println(Gs)
+
+    Gs.toDouble
+  }
+
+  def Fs(solution: Assignment) : Double = {
+    friendsFoes(solution)
+  }
+
+  def friendsFoes(solution: Assignment) : Double = {
+    solution.groupMap.keys.map { key =>
+      val score = for {
+        studId1 :: studId2 :: _ <- solution.studentMap.filter(_._2 == key).keys.toList.combinations(2)
+        student1 = students(studId1)
+        student2 = students(studId2)
+      } yield {
+        val friends = student1.friends.contains(studId2) || student2.friends.contains(studId1)
+        val foes = student1.foes.contains(studId2) || student2.foes.contains(studId1)
+        if(friends && foes) {
+          -.5
+        } else if(friends) {
+          if(student1.friends.contains(studId2) && student2.friends.contains(studId1)) 1
+          else .5
+        } else if(foes) {
+          if(student1.foes.contains(studId2) && student2.foes.contains(studId1)) -2
+          else -1
+        } else {
+          0
+        }
+      }
+
+      score.sum
+    }.sum
+  }
+
 }
