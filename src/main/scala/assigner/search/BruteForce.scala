3dd19e4ce7237ccecdf020f18dad7400a543423a
Georgi Krastev <joro.kr.21@gmail.com>
2015-07-10 09:39:40 +0200

    Solve BruteForce memory issue by using an Iterator

--- src/main/scala/assigner/search/BruteForce.scala
+++ src/main/scala/assigner/search/BruteForce.scala
@@ -17,26 +17,26 @@ class BruteForce(course: Course) {
   /** Recursive function to generate all possible [[Assignment]]s. */
   def assign(
       students: SortedMap[StudentId, Student] = students,
-      groups:   SortedMap[GroupId,   Group]   = groups): Stream[Assignment] =
+      groups:   SortedMap[GroupId,   Group]   = groups): Iterator[Assignment] =
     if (groups.isEmpty ||
         students.size < groups.values.map { _.minSize }.min) {
       if (students.values.exists { _.mandatory } ||
-            groups.values.exists { _.mandatory }) Stream.empty else {
+            groups.values.exists { _.mandatory }) Iterator.empty else {
         val studentMap = students mapValues { _ => -1l }
         val groupMap   = groups   mapValues { _ => SortedSet.empty[StudentId] }
         val queue      = default.queueId -> students.keySet
-        Assignment(studentMap, groupMap + queue) #:: Stream.empty
+        Iterator(Assignment(studentMap, groupMap + queue))
       }
     } else {
       for {
-        (g, group) <- groups.toStream
+        (g, group) <- groups.iterator
         size       <- group.minSize to (group.maxSize min students.size)
         selection  <- students.keySet subsets size
         assignment <- assign(students -- selection, groups - g)
         studentMap  = assignment.studentMap ++ selection.map { _ -> g }
         groupMap    = assignment.groupMap    + (g -> selection)
       } yield Assignment(studentMap, groupMap)
-    }.distinct
+    }
 
   def solution = assign() maxBy objective.score
 }
